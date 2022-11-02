0e67b9da41dda556eacc98d310c5e6577e2d5153
nateeDev <samaneranateechai@gmail.com>
2022-11-02 14:12:26 +0700

    dd

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,71 +1,81 @@
-import React, { useRef, useState } from "react";
+import React, { createRef, useRef, useState } from "react";
 import "./Nav.css";
 
-
-interface IDrops {
-   name: string
-   lists: string[]
+interface DropdownType {
+  title: string;
+  navItem: string[];
 }
 
-interface list {
-  list: string[]
-}
-const itemsDorp:React.FC<list> = (props) => {
-   const { list } = props;
-   return (
-    <nav className="navDropdown">
-      <ul className="nav-linked">
-        {
-          list.map( item => <li>{item}</li>)
-        }
-      </ul>
-    </nav>
-  );
-};
-
-const Dropdowns: React.FC<IDrops> = (props) => {
-  
-  const [toggle, setToggle] = useState(false)
+function dropdown(item: string[]) {
+  const dropRef:any = createRef();
+  const display = (e:any) => {
+    e.target.classList.toggle("display");
+  }
   return (
-    <div>
+    <>
+      <ul ref={dropRef} onMouseEnter={display} onMouseLeave={display} className="">
+        {item.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+const NavDorpdown: React.FC<DropdownType> = (props) => {
+  const [toggle, setToggle] = useState(false);
+  const dropdownRef: any = createRef();
+
+  const toggleDropdown = (e: any) => {
+    dropdownRef.current.classList.toggle("display");
+    dropdownRef.current.classList.toggle("border-line")
+  };
+  return (
+    <div className="nav-dropdown">
       <div
-        className="l"
-        onMouseLeave={() => setToggle(false)}
+        className="toggle-link"
+        onMouseEnter={toggleDropdown}
+        onMouseLeave={toggleDropdown}
       >
-        {props.name}
+        {props.title}
       </div>
-      <itemsDorp list={props.lists} />
+      <nav ref={dropdownRef} className="dropdown">
+        {dropdown(props.navItem)}
+      </nav>
     </div>
   );
 };
 
+const DropdownItems: Array<DropdownType> = [
+  {
+    title: "For Partners",
+    navItem: ["Employers", "Insurers", "Health Systems"],
+  },
+  {
+    title: "For Paytients",
+    navItem: ["Start Here", "What is an HPA?"],
+  },
+];
+
 function Navbar() {
   const [toggle, setToggle] = useState(false);
-  const Partners = ["Employers", "insurers", "Health Systems",];
-
-  const Drops:Array<IDrops> = [
-    {
-      name: "For Partners",
-      lists: Partners
-    }
-  ]
-
-
   const link_Logo = `https://assets.website-files.com/631eddfd322acf4bde169f3f/631ef9dda559623779ccc8fa_paytient-logo.svg`;
 
   return (
     <div className="navbar">
-      <div className="log0">
+      <div className="logo">
         <img src={link_Logo} alt="paytient-logo" />
       </div>
       <div className="nav">
-        <div className="g-d">
-          {
-            Drops.map( item => <Dropdowns name={item.name} lists={item.lists} /> )
-          }
+        <div className="nav-linked">
+          {DropdownItems.map((item) => (
+            <NavDorpdown
+              title={item.title}
+              navItem={item.navItem}
+              key={item.title}
+            />
+          ))}
         </div>
-
-        
         <div className="btn-login"></div>
       </div>
     </div>
